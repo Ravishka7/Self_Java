f34d421aca87514a2c6010a8923d4002b6781165
Ravishka Sulakshana <107683747+Ravishka7@users.noreply.github.com>
2023-11-26 02:16:17 +0530

    Add files via upload

--- learning-java-test3/src/com/ravi3/Main.java
+++ learning-java-test3/src/com/ravi3/Main.java
@@ -1,6 +1,7 @@
 package com.ravi3;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -14,24 +15,35 @@ public class Main {
 //        }
 //        System.out.println("Count is: " + count);
 
-       Scanner scanner1 = new Scanner(System.in);
-       System.out.println("What is your name?");
-       String name = scanner1.nextLine();
+//       Scanner scanner1 = new Scanner(System.in);
+//       System.out.println("What is your name?");
+//       String name = scanner1.nextLine();
+//
+//       System.out.println("What is your age?");
+//       int age = scanner1.nextInt();
+//       int year = LocalDate.now().minusYears(age).getYear();
+//
+//       System.out.println("Hey " +name+ ".Cool you are " +age+ ".Born in " +year+ " huh?");
+//
+//       if (age >= 21) {
+//           System.out.println("So you can drink.");
+//       }else {
+//           System.out.println("Sorry dude just wait for " +(21-age)+ " years.");
+//       }
 
-       System.out.println("What is your age?");
-       int age = scanner1.nextInt();
-       int year = LocalDate.now().minusYears(age).getYear();
+        char [] letters = {'a', 'a', 'c', 'b', 'b', 'b'};
 
-       System.out.println("Hey " +name+ ".Cool you are " +age+ ".Born in " +year+ " huh?");
+        int count = countOccurence(letters, 'a');
+        System.out.println(count);
+    }
 
-       if (age >= 21) {
-           System.out.println("So you can drink.");
-       }else {
-           System.out.println("Sorry dude just wait for " +(21-age)+ " years.");
-       }
-
-
-
-
+    public static int countOccurence(char [] letters, char searchLetter) {
+        int count = 0;
+        for (char letter : letters) {
+            if (letter == searchLetter) {
+                count++;
+            }
+    }
+        return count;
     }
 }
